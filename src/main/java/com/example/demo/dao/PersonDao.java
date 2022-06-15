3a06629358d2c32adf662e4c92dcbb82d7af4e0a
AlessandroB96 <abelaj7196@gmail.com>
2022-06-15 15:46:48 -0400

    added insertPerson method tie to DB

--- src/main/java/com/example/demo/dao/PersonDao.java
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -10,7 +10,7 @@ public interface PersonDao {
 //  we are adding 2 methods. 1 to insert a person into the database with an id, and another to add a person without an id(create an id for us)
     int insertPerson(UUID id, Person person);
 
-    defualt int addPerson(Person person) {
+    default int addPerson(Person person) {
         UUID id = UUID.randomUUID();
         return insertPerson(id, person);
     }
